13103c6ab9b9c0c17cf14323d38d3bc25c8ec089
MartinCaparros <martincaparross@gmail.com>
2021-08-21 12:29:39 -0300

    fix add() limit under stock avaible

--- src/views/components/ItemCount/index.js
+++ src/views/components/ItemCount/index.js
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import { Container, Button, QuantityDiv} from './styled'
 
 const ItemCount = () => {
-
+    const stock = 5;
     const [count, setCount] = useState(0)
 
     const add = () => {
+        if (!(count >= stock))
         return setCount(count + 1)
     }
     const substract = () => {
